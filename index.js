f9eb836b73b16b562568cb5e5d35d59e5b5eece5
Brian Mulindi <mulindijrn@gmail.com>
2023-09-18 18:51:33 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,28 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+  
+let multiply = num1 * num2;
+console.log(multiply);
+
+function getRandomIntegerGreaterThanZero() {
+    let randomFloat = Math.random();
+  
+    let randomInteger = Math.floor(randomFloat * Number.MAX_SAFE_INTEGER) + 1;
+  
+    return randomInteger;
+}
+  
+let random = getRandomIntegerGreaterThanZero();
+  
+console.log(random);
+
+let num3 = 12;
+let num4 = 8;
+
+let mod = num3 % num4;
+
+let numbers = [5, 12, 3, 20, 9];
+
+let max = Math.max(...numbers);
